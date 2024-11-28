1e02a40f368c861fedc94a915f6a79bef7a1c120
baiyinqin <baiyinqinming@outlook.com>
2024-11-28 14:35:33 +0800

    启动项目

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	@import '@/tailwindcss.scss';
+	page {
+		// min-height: 100vh;
+		// font-size: 24rpx;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<view class="w-100 b h-100">
+
+	</view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
